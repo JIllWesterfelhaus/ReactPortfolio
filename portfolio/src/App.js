9d35e73510cb2717d5ad04cd6e46342b14e4e458
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-06-26 09:12:00 -0500

    added bootstrap code, updated Header and Navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './index.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="Header">
         
       </header>
     </div>
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../styles/navbar.css";
+
+function navbar() {
+    return(<nav>Jill Westerfelhaus
+        <div></div>
+    </nav>)
+}
+export default navbar
