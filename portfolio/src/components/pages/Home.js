df986540baa076379fb534172a2d0e7cd3f14f55
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-06-27 09:51:45 -0500

    updated aboutme, home, css

--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -5,7 +5,9 @@ function Home() {
       <div>
         <h1>Hi!</h1>
         <p>
-          Home page paragraph here.
+        <h5 class="text-center">I am a student at Northwestern University's Coding Bootcamp.</h5>  
+    <h5 class="text-center">I'm learning Full Stack Web Development.</h5>
+    <h5 class="text-center">Please take a look around and see what I'm learning!</h5>
         </p>
       </div>
     );
