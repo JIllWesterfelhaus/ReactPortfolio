267b0cdc23306fab0cd37f5be5f9cbd5a112dc48
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-06-28 12:58:46 -0500

    added photo to Home.js

--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Headshot from "./images/JW headshot.png";
  
 function Home() {
     return (
       <div>
         <h1>Hi!</h1>
         <p>
+        <img style={{width: "40%"}} src={Headshot} alt="Jill Westerfelhaus" />
         <h5 class="text-center">I am a student at Northwestern University's Coding Bootcamp.</h5>  
     <h5 class="text-center">I'm learning Full Stack Web Development.</h5>
     <h5 class="text-center">Please take a look around and see what I'm learning!</h5>
