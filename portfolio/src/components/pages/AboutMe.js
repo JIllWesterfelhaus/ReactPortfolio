cbfb9e15661cf237410593810273984735397750
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-06-28 12:56:48 -0500

    added photo to About Me page

--- portfolio/src/components/pages/AboutMe.js
+++ portfolio/src/components/pages/AboutMe.js
@@ -1,14 +1,13 @@
 import React from "react";
+import Headshot from "./images/JW headshot.png";
 
-
- 
 function About() {
     return (
-      
        
         <div>
         <h1>About Me</h1>
         <div class="container aboutMe">
+        <img style={{width: "25%"}} src={Headshot} alt="Jill Westerfelhaus" />
         <div class="row d-flex justify-content-center"> 
     <p>I am currently a student at Northwestern University's Coding and Full Stack Web Development bootcamp. What I like about development is the moment when you click a button and a real working webpage that you made pops up. It reminds me of my days working in darkroom photography, watching an image come up in the tray of developer, under the glow of a safelight.</p>
        <p>I have worked as a darkroom photo lab tech, and now I am studying web development. In between, I was a Licensed Clinical Professional Counselor, conducting assessments on people with severe mental illness. I bring skills in listening and working with people to web development, as well as the aesthetics and technology of photography. I am excited to bring all my skills and knowledge to this new field. </p>
@@ -20,7 +19,7 @@ function About() {
     <h5 class="text-center"><a href="https://github.com/JIllWesterfelhaus">GitHub</a></h5>
     <h5 class="text-center"><a href="https://drive.google.com/file/d/1uI29ld_OinWKOvuB_AKhaZKmx78qHQEb/view?usp=sharing">Resume</a></h5>   
 </div>
-    );
+); 
   }
   
   export default About;
