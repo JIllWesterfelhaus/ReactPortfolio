6e884414e84dbc334f5313a88b1e3985a86d8ffe
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-06-28 12:59:58 -0500

    Update Home.js

--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
       <div>
         <h1>Hi!</h1>
         <p>
-        <img style={{width: "40%"}} src={Headshot} alt="Jill Westerfelhaus" />
+        <img style={{width: "35%"}} src={Headshot} alt="Jill Westerfelhaus" />
         <h5 class="text-center">I am a student at Northwestern University's Coding Bootcamp.</h5>  
     <h5 class="text-center">I'm learning Full Stack Web Development.</h5>
     <h5 class="text-center">Please take a look around and see what I'm learning!</h5>
