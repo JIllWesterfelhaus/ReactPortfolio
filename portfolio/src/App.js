b4d1f9885c829c60cafb77786d5b6578ff67fb0e
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-06-30 12:04:38 -0500

    added header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,16 +5,19 @@ import Home from "./components/pages/Home";
 import AboutMe from "./components/pages/AboutMe";
 import Portfolio from "./components/pages/Portfolio";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
       <div>
         <Navtabs />
+        <Header/>
         <Route exact path="/home" component={Home} />
         <Route exact path="/about" component={AboutMe} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Footer/>
+        
       </div>
     </Router>
   );
