dfbe5bb306994a358f5fc8ab8568ba1d83305b88
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-06-30 16:13:41 -0500

    updated class to className

--- portfolio/src/components/pages/AboutMe.js
+++ portfolio/src/components/pages/AboutMe.js
@@ -15,11 +15,11 @@ function About() {
        <p>I have worked as a darkroom photo lab tech, and now I am studying web development. In between, I was a Licensed Clinical Professional Counselor, conducting assessments on people with severe mental illness. I bring skills in listening and working with people to web development, as well as the aesthetics and technology of photography. I am excited to bring all my skills and knowledge to this new field. </p>
     </div>
 </div>
-<h5 class="text-center">Email:  jillwesthaus@gmail.com</h5>  
-    <h5 class="text-center">Phone:  773-592-7923</h5>
-    <h5 class="text-center"><a href="https://www.linkedin.com/in/jill-westerfelhaus-a821761a/">LinkedIn</a></h5>
-    <h5 class="text-center"><a href="https://github.com/JIllWesterfelhaus">GitHub</a></h5>
-    <h5 class="text-center"><a href="https://drive.google.com/file/d/1uI29ld_OinWKOvuB_AKhaZKmx78qHQEb/view?usp=sharing">Resume</a></h5>   
+<h5 className="text-center">Email:  jillwesthaus@gmail.com</h5>  
+    <h5 className="text-center">Phone:  773-592-7923</h5>
+    <h5 className="text-center"><a href="https://www.linkedin.com/in/jill-westerfelhaus-a821761a/">LinkedIn</a></h5>
+    <h5 className="text-center"><a href="https://github.com/JIllWesterfelhaus">GitHub</a></h5>
+    <h5 className="text-center"><a href="https://drive.google.com/file/d/1uI29ld_OinWKOvuB_AKhaZKmx78qHQEb/view?usp=sharing">Resume</a></h5>   
 </div>
 ); 
   }
--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -6,15 +6,15 @@ function Home() {
       <div className="homepage">
         <h1>Hi!</h1>
         
-        <div classname="Headshot"style={{display: "flex", justifyContent: "center"}}>
+        <div className="Headshot"style={{display: "flex", justifyContent: "center"}}>
         <img className="rounded-circle"style={{width: "35%"}} src={Headshot} alt="Jill Westerfelhaus" />
         </div>
         
-        <h5 class="text-center">I am a student at Northwestern University's Coding Bootcamp.</h5>  
-    <h5 class="text-center">I'm learning Full Stack Web Development.</h5>
-    <h5 class="text-center">Please take a look around and see what I'm learning!</h5>
-      </div>
+        <h5 className="text-center">I am a student at Northwestern University's Coding Bootcamp.</h5>  
+    <h5 className="text-center">I'm learning Full Stack Web Development.</h5>
+    <h5 className="text-center">Please take a look around and see what I'm learning!</h5>
       
+      </div>
       
     );
   }
