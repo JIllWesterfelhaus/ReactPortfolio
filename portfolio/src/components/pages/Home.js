cf1d9705cc85b29c22ea2531325b10b1f2e8946d
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-06-30 09:12:08 -0500

    attempting to center photo, home and about me

--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -5,13 +5,13 @@ function Home() {
     return (
       <div>
         <h1>Hi!</h1>
-        <p>
+        <div classname="Headshot">
         <img className="rounded-circle"style={{width: "35%"}} src={Headshot} alt="Jill Westerfelhaus" />
-        
+        </div>
         <h5 class="text-center">I am a student at Northwestern University's Coding Bootcamp.</h5>  
     <h5 class="text-center">I'm learning Full Stack Web Development.</h5>
     <h5 class="text-center">Please take a look around and see what I'm learning!</h5>
-        </p>
+      
       </div>
     );
   }
