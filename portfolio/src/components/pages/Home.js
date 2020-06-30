12e72a5c7804faa0ac280306030a3cc246503c0b
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-06-30 12:40:36 -0500

    centered photo on Home page

--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -10,14 +10,10 @@ function Home() {
         <img className="rounded-circle"style={{width: "35%"}} src={Headshot} alt="Jill Westerfelhaus" />
         </div>
         
-        
-        
         <h5 class="text-center">I am a student at Northwestern University's Coding Bootcamp.</h5>  
     <h5 class="text-center">I'm learning Full Stack Web Development.</h5>
     <h5 class="text-center">Please take a look around and see what I'm learning!</h5>
-      
       </div>
-      
       
     );
   }
