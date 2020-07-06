612459d589b263e6dc182f40e7376a7474484b5b
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-07-06 18:30:41 -0500

    updated multiple files

---
+++ portfolio/build/precache-manifest.3a5697abcb820c161e4b1762d2a52bdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233d5348964a2a0da12d25844116731f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3502caacf7281f0a826d",
+    "url": "/static/css/main.2ec6ab96.chunk.css"
+  },
+  {
+    "revision": "7414e85ee124326f4d00",
+    "url": "/static/js/2.d0254ae4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d0254ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3502caacf7281f0a826d",
+    "url": "/static/js/main.a39cca56.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5d307002a5d66f64591cc990ff544a92",
+    "url": "/static/media/JW headshot.5d307002.png"
+  }
+]);
