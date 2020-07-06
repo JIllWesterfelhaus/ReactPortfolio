3e937dc12730d8fa542e00e42197e3dc0acbab79
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-07-06 18:01:54 -0500

    Update server.js

--- portfolio/server.js
+++ portfolio/server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+
+if (process.env.NODE_ENV === "production")
+app.use(express.static("client/build"));
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
