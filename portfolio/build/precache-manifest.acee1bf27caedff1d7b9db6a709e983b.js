a1d6a83654866bfd30f9ed3ce3f4200b78eeb3c0
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-07-06 20:08:21 -0500

    updated files

---
+++ portfolio/build/precache-manifest.acee1bf27caedff1d7b9db6a709e983b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0b12d0741c772ee14798a420e7099a",
+    "url": "/JIllWesterfelhaus/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "9c1ba2cec8d7308aa8a3",
+    "url": "/JIllWesterfelhaus/ReactPortfolio/static/css/main.2ec6ab96.chunk.css"
+  },
+  {
+    "revision": "7414e85ee124326f4d00",
+    "url": "/JIllWesterfelhaus/ReactPortfolio/static/js/2.d0254ae4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JIllWesterfelhaus/ReactPortfolio/static/js/2.d0254ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1ba2cec8d7308aa8a3",
+    "url": "/JIllWesterfelhaus/ReactPortfolio/static/js/main.d149f010.chunk.js"
+  },
+  {
+    "revision": "33584deeca04db0e865c",
+    "url": "/JIllWesterfelhaus/ReactPortfolio/static/js/runtime-main.a9b4590a.js"
+  },
+  {
+    "revision": "5d307002a5d66f64591cc990ff544a92",
+    "url": "/JIllWesterfelhaus/ReactPortfolio/static/media/JW headshot.5d307002.png"
+  }
+]);
--- portfolio/build/service-worker.js
+++ portfolio/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a5697abcb820c161e4b1762d2a52bdd.js"
+  "/JIllWesterfelhaus/ReactPortfolio/precache-manifest.acee1bf27caedff1d7b9db6a709e983b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JIllWesterfelhaus/ReactPortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
